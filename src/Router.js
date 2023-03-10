fa58699859b6798a5760ad349f4e6474de037b4e
Hectorv72 <hectorvaldezfsa13@gmail.com>
2023-03-10 11:49:48 -0300

    modifica la estructura de navegacion
    elimina el tab, vistas y arcivos innecesarios

--- src/Router.js
+++ src/Router.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { app } from './config'
-import Tabs from './Tabs';
 import Game from './views/Game';
+import Home from './views/Home';
 
 const Router = () => {
 
   const { Navigator, Screen } = createNativeStackNavigator();
 
   const views = [
-    { name: "main", component: Tabs, options: { title: app.appName } },
+    { name: "main", component: Home, options: { title: app.appName } },
     { name: "game", component: Game, options: { headerShown: false } },
     // { name: "", component: Component, options: {} },
   ]
